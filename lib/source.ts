4146f4ef6c5058fad4c1ec64a967393ae9a1c7f4
Chunn20 <964627717@qq.com>
2026-04-14 15:13:15 +0800

    添加IconResolver支持，左侧导航栏使用Lucide矢量图标

--- lib/source.ts
+++ lib/source.ts
@@ -1,7 +1,17 @@
 import { docs } from '@/.source/server';
 import { loader } from 'fumadocs-core/source';
+import { createElement } from 'react';
+import { icons } from 'lucide-react';
 
 export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
+  icon(icon) {
+    if (!icon) return;
+    if (icon in icons) {
+      return createElement(icons[icon as keyof typeof icons], {
+        className: 'h-5 w-5 text-[#FF8A00]',
+      });
+    }
+  },
 });
